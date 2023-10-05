3e296f4f9260ba49280920b9583b45a15f7b07c5
Gaurav <gauravsomani52750@gmail.com>
2023-10-05 18:26:02 +0530

    added Ourexpert component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./views/Navbar";
 import Home from "./views/Home";
 import Footer from "./components/Footer";
+import OurExpert from "./components/OurExpert";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <OurExpert />
       <Footer />
     </div>
   );
