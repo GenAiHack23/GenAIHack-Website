eaaa7266da52890dc0e3a9a88c3806cf6437d071
Tejas Chaudhari <tejasc888@gmail.com>
2023-10-05 18:10:24 +0530

    added missing href

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
           <h1 className='links-title'>Links</h1>
           <a className='link' href='https://www.instagram.com/genaihack/'>Instagram</a>
           <a className='link' href='https://twitter.com/GenAiHack'>Twitter</a>
-          <a className='link'>Sponsor Us</a>
+          <a className='link' href='https://https://docs.google.com/forms/'>Sponsor Us</a>
         </div>
         <div className='get-in-touch col-span-2'>
           <h1 className='get-in-touch-title'>Get in touch</h1>
