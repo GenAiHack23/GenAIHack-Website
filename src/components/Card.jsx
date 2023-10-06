7ab7ce1eac0a8c036106e86a32d350ad77e80534
Gaurav <gauravsomani52750@gmail.com>
2023-10-06 18:23:06 +0530

    characteristic component responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import "./chars.css";
 const Card = ({ title, description }) => {
   return (
     <>
-      <div className="block max-w-sm p-6  border-gray-200 rounded-lg shadow bg-black">
+      <div className="flex-1 text-left lg:max-w-sm p-6  border-gray-200 rounded-lg shadow bg-black ">
         <hr className="border-0 bg-gradient-to-r from-purple-600 to-orange-300 w-11 h-1 py-0 rounded-full mb-3" />
         <h5 className="heading-color mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
--- src/components/Characteristics.jsx
+++ src/components/Characteristics.jsx
@@ -5,16 +5,19 @@ import Card from "./Card";
 export const Characteristics = () => {
   const characteristics = [
     {
+      id: 1,
       title: "Foster Innovation",
       description:
-        "Hackathons encourage participants to push the boundaries of generative AI technology, leading to new ideas and creative solutions.",
+        "They provide a platform for participants to enhance their AI and coding skills, learn from peers, and gain practical experience.",
     },
     {
+      id: 2,
       title: "Skill Development",
       description:
         "They provide a platform for participants to enhance their AI and coding skills, learn from peers, and gain practical experience.",
     },
     {
+      id: 3,
       title: "Address Real",
       description:
         "Challenge participants to create AI solutions for real-world problems or generate valuable content.",
@@ -23,11 +26,13 @@ export const Characteristics = () => {
 
   const characteristics2 = [
     {
+      id: 4,
       title: "Cross-Generation Learning",
       description:
         "Promote collaboration between college students and corporate professionals to bridge the gap between academia and industry.",
     },
     {
+      id: 5,
       title: "Raise Ethical Awareness",
       description:
         "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
@@ -37,27 +42,35 @@ export const Characteristics = () => {
   //
   return (
     <>
-      <div className="chars-div mb-24">
-        <div className="heading">Characteristics</div>
+      <div className="lg:flex lg:justify-center">
+        <div className="chars-div  mb-24 text-center lg:w-11/12  p-10">
+          <div className="heading text-3xl md:text-5xl lg:text-7xl font-extrabold ">
+            Characteristics
+          </div>
 
-        <div className="flex space-x-5 mt-14">
-          {characteristics.map((char) => {
-            return (
-              <div>
-                <Card title={char.title} description={char.description} />
-              </div>
-            );
-          })}
-        </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:flex justify-center lg:space-x-5 mt-14 ">
+            {characteristics.map((char) => {
+              return (
+                <Card
+                  key={char.id}
+                  title={char.title}
+                  description={char.description}
+                />
+              );
+            })}
+          </div>
 
-        <div className="flex space-x-5 pt-16">
-          {characteristics2.map((char) => {
-            return (
-              <div>
-                <Card title={char.title} description={char.description} />
-              </div>
-            );
-          })}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:flex justify-center lg:space-x-5 pt-16">
+            {characteristics2.map((char) => {
+              return (
+                <Card
+                  key={char.id}
+                  title={char.title}
+                  description={char.description}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
