4d4836fe3a0e57328e636e6a1c15b253f2d8f20c
Mikhiel <mikhielbenji9@gmail.com>
2023-10-07 16:54:04 +0530

    Chnages in About us page

--- src/views/about_us.jsx
+++ src/views/about_us.jsx
@@ -1,26 +1,37 @@
-
+import React, { useState } from "react";
 import "./about-us.css";
 
+const ReadMore = ({ children }) => { 
+  const text = children; 
+  const [isReadMore, setIsReadMore] = useState(true); 
+  const toggleReadMore = () => { 
+    setIsReadMore(!isReadMore); 
+  }; 
+  return ( 
+    <p className="text"> 
+      {isReadMore ? text.slice(0, 150) : text} 
+      <span onClick={toggleReadMore} className="read-or-hide"> 
+        {isReadMore ? "...read more" : " show less"} 
+      </span> 
+    </p> 
+  ); 
+}; 
 
-export default function Aboutus() {
+function AboutUs() {
     return (
         <div className="frame">
             <div className="group2">
                 <div className="overlap-group2">
                     <p className="genai-short-for">
-                        &#34;GenAI,&#34; short for &#34;Generative AI,&#34; represents a category of artificial intelligence systems
+                        <ReadMore> 
+                        "GenAI," short for "Generative AI," represents a category of artificial intelligence systems
                         capable of autonomously generating various forms of content, such as text, images, music, or software. These
                         systems leverage advanced techniques like neural networks and deep learning to produce creative, realistic,
                         or human-like content. GPT-3, the model powering this conversation, serves as an example of GenAI, excelling
                         in natural language understanding and generation. GenAI finds applications across diverse domains, including
                         chatbots, content generation, image synthesis, and more, and continues to evolve as a pivotal field in
-                        AI&#34;GenAI,&#34; short for &#34;Generative AI,&#34; represents a category of artificial intelligence
-                        systems capable of autonomously generating various forms of content, such as text, images, music, or
-                        software. These systems leverage advanced techniques like neural networks and deep learning to produce
-                        creative, realistic, or human-like content. GPT-3, the model powering this conversation, serves as an
-                        example of GenAI, excelling in natural language understanding and generation. GenAI finds applications
-                        across diverse domains, including chatbots, content generation, image synthesis, and more, and continues to
-                        evolve as a pivotal field in AI
+                        AI.
+                        </ReadMore>
                     </p>
                     <div className="ellipse" />
                     <div className="heading2">About US</div>
@@ -28,4 +39,6 @@ export default function Aboutus() {
             </div>
         </div>
     );
-};
+}
+
+export default AboutUs;
