bf6ae04ffb0989444fe76ed7f0e61fdddcbc1c53
Gaurav <gauravsomani52750@gmail.com>
2023-10-08 10:44:59 +0530

    alignment issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Navbar from "./views/Navbar";
 import Home from "./views/Home";
 import Footer from "./components/Footer";
 import OurExpert from "./components/OurExpert";
-import AboutUs from "./views/AboutUs";
+// import AboutUs from "./views/AboutUs";
 import { Characteristics } from "./components/Characteristics";
 import Timeline from "./components/Timeline";
+import AboutUs from "./views/about_us";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      {/* <AboutUs /> */}
       <AboutUs />
       <Characteristics />
       <OurExpert />
--- src/views/about_us.jsx
+++ src/views/about_us.jsx
@@ -1,44 +1,51 @@
 import React, { useState } from "react";
 import "./about-us.css";
 
-const ReadMore = ({ children }) => { 
-  const text = children; 
-  const [isReadMore, setIsReadMore] = useState(true); 
-  const toggleReadMore = () => { 
-    setIsReadMore(!isReadMore); 
-  }; 
-  return ( 
-    <p className="text"> 
-      {isReadMore ? text.slice(0, 150) : text} 
-      <span onClick={toggleReadMore} className="read-or-hide"> 
-        {isReadMore ? "...read more" : " show less"} 
-      </span> 
-    </p> 
-  ); 
-}; 
+const ReadMore = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <p className="text">
+      {isReadMore ? text.slice(0, 150) : text}
+      <span onClick={toggleReadMore} className="read-or-hide">
+        {isReadMore ? "...read more" : " show less"}
+      </span>
+    </p>
+  );
+};
 
 function AboutUs() {
-    return (
-        <div className="frame">
-            <div className="group2">
-                <div className="overlap-group2">
-                    <p className="genai-short-for">
-                        <ReadMore> 
-                        "GenAI," short for "Generative AI," represents a category of artificial intelligence systems
-                        capable of autonomously generating various forms of content, such as text, images, music, or software. These
-                        systems leverage advanced techniques like neural networks and deep learning to produce creative, realistic,
-                        or human-like content. GPT-3, the model powering this conversation, serves as an example of GenAI, excelling
-                        in natural language understanding and generation. GenAI finds applications across diverse domains, including
-                        chatbots, content generation, image synthesis, and more, and continues to evolve as a pivotal field in
-                        AI.
-                        </ReadMore>
-                    </p>
-                    <div className="ellipse" />
-                    <div className="heading2">About US</div>
-                </div>
-            </div>
+  return (
+    <div className="frame">
+      <div className="group2">
+        <div className="overlap-group2 text-center ">
+          <p className="genai-short-for">
+            <ReadMore>
+              "GenAI," short for "Generative AI," represents a category of
+              artificial intelligence systems capable of autonomously generating
+              various forms of content, such as text, images, music, or
+              software. These systems leverage advanced techniques like neural
+              networks and deep learning to produce creative, realistic, or
+              human-like content. GPT-3, the model powering this conversation,
+              serves as an example of GenAI, excelling in natural language
+              understanding and generation. GenAI finds applications across
+              diverse domains, including chatbots, content generation, image
+              synthesis, and more, and continues to evolve as a pivotal field in
+              AI.
+            </ReadMore>
+          </p>
+          <div className="ellipse" />
+          {/* <div className="heading2">About US</div> */}
+          <div className="heading text-3xl md:text-5xl lg:text-7xl font-extrabold">
+            About US
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
