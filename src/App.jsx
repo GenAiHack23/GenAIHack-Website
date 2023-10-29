b19b17b0d286e84a8f6b61b0b43b576922484683
asp21k <atharva.pardeshi0023@gmail.com>
2023-10-29 10:47:22 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Navbar from "./views/navbar";
 import Home from "./views/Home";
 import Footer from "./components/Footer";
 import OurExpert from "./components/OurExpert";
-import AboutUs from "./views/AboutUs";
+// import AboutUs from "./views/AboutUs";
 import { Characteristics } from "./components/Characteristics";
 import Timeline from "./components/Timeline";
+import AboutUs from "./views/about_us";
 
 export default function App() {
   return (
     <div> 
       <Navbar />
       <Home />
-      <AboutUs />
+      {/* <AboutUs /> */}
+      {/* <AboutUs /> */}
       <Characteristics />
       <OurExpert />
       <Timeline />
