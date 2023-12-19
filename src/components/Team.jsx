82f9435d5cd88332e31a21f10c8c3ce297b16d08
Mikhiel <mikhielbenji9@gmail.com>
2023-12-19 20:48:00 +0530

    Made the meet my team component responsive

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
 import '../views/about.css';
-import person1 from "../assets/person1.png";
 
-function Team() {
+function Team(props) {
   return (
-    <div className="w-1/5 rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={person1} alt="Sunset in the mountains" />
-            <div className="px-2 py-2">
-              <h1 className='font-mono text-sm text-slate-300 mt-3 mb-7'>Frontend Developer</h1>
-              <div className="font-bold text-xl mb-2">Mary Rose</div>
-            </div>
-            <div className="px-2 pt-5 pb-4">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-            </div>
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={props.image} alt={props.name} />
+      <div className="px-2 py-2 sm:text-xs">
+        <h1 className="font-mono text-sm text-slate-300 mt-3 mb-1 sm:mb-2">{props.title}</h1>
+        <div className="font-bold text-xl mb-2 sm:text-xs">{props.name}</div>
+      </div>
+      <div className="px-2 pt-2 pb-4 sm:text-xs">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Team;
+
+
+
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,9 +1,12 @@
 import './about.css';
 import Team from "../components/Team";
+import Rules from "../components/Rules"
+import Steps from "../components/Steps"
+import person1 from "../assets/person1.png"
 import { useState } from 'react';
 
 export default function AboutView() {
-  const [dev, setDev] = useState(0);
+  const [dev, setDev] = useState(1);
   const [content, setContent] = useState(0);
   const [organizers, setOrganizers]=useState(0);
    const [design, setDesign] = useState(0);
@@ -47,86 +50,32 @@ export default function AboutView() {
       <h1 className='title'>Steps to Register</h1>
       <div style={{ height: 25 }}></div>
       <div className='grid sm:grid-cols-3 gap-4 text-center grid-cols-1'>
-        <div className="card">
-          <h1 className="title">1</h1>
-          <div style={{ height: 15 }}></div>
-          <p>The quick brown fox jumped over the lazy dog.</p>
-        </div>
-        <div className="card">
-          <h1 className="title">2</h1>
-          <div style={{ height: 15 }}></div>
-          <p>The quick brown fox jumped over the lazy dog.</p>
-        </div>
-        <div className="card">
-          <h1 className="title">3</h1>
-          <div style={{ height: 15 }}></div>
-          <p>The quick brown fox jumped over the lazy dog.</p>
-        </div>
-        <div className="card">
-          <h1 className="title">4</h1>
-          <div style={{ height: 15 }}></div>
-          <p>The quick brown fox jumped over the lazy dog.</p>
-        </div>
-        <div className="card">
-          <h1 className="title">5</h1>
-          <div style={{ height: 15 }}></div>
-          <p>The quick brown fox jumped over the lazy dog.</p>
-        </div>
-        <div className="card">
-          <h1 className="title">6</h1>
-          <div style={{ height: 15 }}></div>
-          <p>The quick brown fox jumped over the lazy dog.</p>
-        </div>
+        <Steps number="1" content="The quick brown fox jumped over the lazy dog."/>
+        <Steps number="2" content="The quick brown fox jumped over the lazy dog."/>
+        <Steps number="3" content="The quick brown fox jumped over the lazy dog."/>
+        <Steps number="4" content="The quick brown fox jumped over the lazy dog."/>
+        <Steps number="5" content="The quick brown fox jumped over the lazy dog."/>
+        <Steps number="6" content="The quick brown fox jumped over the lazy dog."/>
       </div>
 
       <div style={{ height: 80 }}></div>
       <div className='text-center'>
         <h1 className='title'>Rules and Regulations</h1>
       </div>
-      <div style={{ height: 25 }}></div>
-      <div className="card">
-        <div className="grid grid-cols-12 items-center">
-          <div className='col-span-2 text-center'>
-            <h1 className="title">1</h1>
-          </div>
-          <div className='col-span-10'>
-            <p>The quick brown fox jumped over the lazy dog.</p>
-          </div>
-        </div>
-      </div>
-      <div style={{ height: 15 }}></div>
-      <div className="card">
-        <div className="grid grid-cols-12 items-center">
-          <div className='col-span-2 text-center'>
-            <h1 className="title">2</h1>
-          </div>
-          <div className='col-span-10'>
-            <p>The quick brown fox jumped over the lazy dog.</p>
-          </div>
-        </div>
-      </div>
-      <div style={{ height: 15 }}></div>
-      <div className="card">
-        <div className="grid grid-cols-12 items-center">
-          <div className='col-span-2 text-center'>
-            <h1 className="title">3</h1>
-          </div>
-          <div className='col-span-10'>
-            <p>The quick brown fox jumped over the lazy dog.</p>
-          </div>
-        </div>
-      </div>
+      <Rules number="1" content="The quick brown fox jumped over the lazy dog."/>
+      <Rules number="2" content="The quick brown fox jumped over the lazy dog."/>
+      <Rules number="3" content="The quick brown fox jumped over the lazy dog."/>
       <div style={{ height: 80 }}></div>
-      <div className="card grid grid-cols-1 items-center sm:grid-cols-4">
-        <div className='col-span-4 text-center'>
+      <div className="card grid grid-cols-1 md:grid-cols-4 items-center mb-8">
+        <div className='md:col-span-4 text-center'>
           <h1 className='title'>Meet our team</h1>
           <div style={{ height: 30 }}></div>
           <h1 className='font-mono text-lg text-slate-500'>Meet our team of professionals to serve you</h1>
           <div style={{ height: 30 }}></div>
-          <div className='flex-row space-x-3 '>
+          <div className='flex flex-wrap justify-center mt-4'>
           <button
           className={`bg-white hover:text-cyan-600 text-slate-500 font-semibold py-2 px-4 border border-gray-400 rounded shadow ${
-            dev === 1 ? 'mr-1' : ''
+            dev === 1 ? 'mr-1' : 'mr-1'
           }`}
           onClick={() => {
             setDev(1);
@@ -140,7 +89,7 @@ export default function AboutView() {
         </button>
         <button
           className={`bg-white hover:text-cyan-600 text-slate-500 font-semibold py-2 px-4 border border-gray-400 rounded shadow ${
-            content === 1 ? 'mr-1' : ''
+            content === 1 ? 'mr-1' : 'mr-1'
           }`}
           onClick={() => {
             setDev(0);
@@ -154,7 +103,7 @@ export default function AboutView() {
         </button>
         <button
           className={`bg-white hover:text-cyan-600 text-slate-500 font-semibold py-2 px-4 border border-gray-400 rounded shadow ${
-            organizers === 1 ? 'mr-1' : ''
+            organizers === 1 ? 'mr-1' : 'mr-1'
           }`}
           onClick={() => {
             setDev(0);
@@ -168,7 +117,7 @@ export default function AboutView() {
         </button>
         <button
           className={`bg-white hover:text-cyan-600 text-slate-500 font-semibold py-2 px-4 border border-gray-400 rounded shadow ${
-            design === 1 ? 'mr-1' : ''
+            design === 1 ? 'mr-1' : 'mr-1'
           }`}
           onClick={() => {
             setDev(0);
@@ -182,7 +131,7 @@ export default function AboutView() {
         </button>
         <button
           className={`bg-white hover:text-cyan-600 text-slate-500 font-semibold py-2 px-4 border border-gray-400 rounded shadow  ${
-            marketing === 1 ? 'mr-1' : ''
+            marketing === 1 ? 'mr-1' : 'mr-1'
           }`}
           onClick={() => {
             setDev(0);
@@ -196,38 +145,38 @@ export default function AboutView() {
         </button>
        </div>
       <div style={{ height: 30 }}></div>
-      <div className='flex self-center space-x-3 '>
+      <div className='flex flex-wrap space-x-3 gap-y-3 items-center justify-center mt-4'>
          {dev === 1 && (
           < >
-            <Team />
-            <Team />
-            <Team />
-            <Team />
-            <Team />
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
           </>
         )}
         {content === 1 && (
           <>
-            <Team />
-            <Team />
-            <Team />
-            <Team />
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
           </>
         )}
         {organizers === 1 && (
           <>
-            <Team />
-            <Team />
-            <Team />
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
           </>
         )}
         {design === 1 && (
           <>
-            <Team />
-            <Team />
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
+            <Team name="Mary Rose" image={person1} title="Frontend Developer"/>
           </>
         )}
-        {marketing === 1 && <Team />}
+        {marketing === 1 && <Team name="Mary Rose" image={person1} title="Frontend Developer"/>}
       </div>
         </div>
         </div>
